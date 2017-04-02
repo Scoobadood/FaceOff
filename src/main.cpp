c538bcc30b4321777b1199bb0afd4761adcb5c8e
Dave Durbin <scoobadood@me.com>
2017-04-02 17:03:06 +1000

    Refactor: Replaced handrolled waitForAKey with Highgui version

--- src/main.cpp
+++ src/main.cpp
@@ -7,49 +7,6 @@
 
 const int SAMPLE_READ_WAIT_TIMEOUT = 2000; // ms
 
-
-// Includes for keyboard polling
-#include <unistd.h>
-#include <stdlib.h>
-#include <termios.h>
-#include <fcntl.h>
-/**
- * Check for keypress
- */
-int wasKeyboardHit()
-{
-	struct termios oldt, newt;
-	int ch;
-	int oldf;
-
-	// don't echo and don't wait for ENTER
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-	
-	// make it non-blocking (so we can check without waiting)
-	if (0 != fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK)) {
-		return 0;
-	}
-
-	ch = getchar();
-
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-	if (0 != fcntl(STDIN_FILENO, F_SETFL, oldf)) {
-		return 0;
-	}
-
-	if(ch != EOF) {
-		ungetc(ch, stdin);
-		return 1;
-	}
-
-	return 0;
-}
-
-
 /** 
  * Setup and initialise openNI
  * or fail
@@ -168,7 +125,7 @@ int main( int argc, char *argv[] ) {
 
 	int numDepthFrames = 0;
 	int numColourFrames= 0;
-	while (!wasKeyboardHit()) {
+	while ( cv::waitKey(10) == -1 )  {
 
 		int frameType = getFrame( depth, colour, frame );
 
