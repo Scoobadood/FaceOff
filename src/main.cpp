d54fd34fd8a7ecb57b86b8141167c5ce077498f4
Dave Durbin <scoobadood@me.com>
2017-04-09 17:07:39 +1000

    feature: Added face detection using Haar cascades

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,17 @@
 #include <OpenNI.h>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
 
 #include "FaceOffConfig.h"
 
 const int SAMPLE_READ_WAIT_TIMEOUT = 2000; // ms
+
+const float FACE_SIZE_RATIO = 0.25f;
+const float EYE_SIZE_RATIO  = 0.25f;
+const float EYE_REGION_HEIGHT_PROPORTION = 0.6f;
+const cv::Scalar BLUE( 255, 0, 0 );
+const cv::Scalar GREEN( 0, 255, 0 );
 
 /** 
  * Setup and initialise openNI
@@ -104,6 +111,48 @@ int getFrame( openni::VideoStream& depth, openni::VideoStream& colour, openni::V
 	return readyStream;
 }
 
+/**
+ * @return the path to the directory containing this executable file
+ */
+int getPathToExe( char * buffer, int buffer_length ) {
+	char szTmp[32];
+	sprintf(szTmp, "/proc/%d/exe", getpid());
+	int bytes = MIN(readlink(szTmp, buffer, buffer_length), buffer_length - 1);
+	if(bytes >= 0)
+    	buffer[bytes] = '\0';
+
+    // Now search from the end of the string to find the last file separator
+    while( (--bytes > 0 ) && buffer[bytes] != '/');
+    if( buffer[bytes] == '/') {
+    	buffer[bytes] = '\0';
+    } else {
+    	bytes = strlen( buffer );
+    }
+
+	return bytes;
+}
+
+/*
+ * Load Haar classifiers for face and eyes
+ */
+bool loadHaarClassifiers( cv::CascadeClassifier& face_classifier, cv::CascadeClassifier& left_eye_classifier, cv::CascadeClassifier& right_eye_classifier ) {
+	// Work out where this code is running from
+	char exe_dir[1024];
+	getPathToExe( exe_dir, 1024 );
+
+	std::string base( exe_dir );
+	base += "/data";
+
+	// Classifiers should be in a data subdirectory
+	std::cout << "Loading haar cascades from " << base << std::endl;
+
+	bool loaded_ok = true;
+	loaded_ok &= face_classifier.load( base + "/haarcascade_frontalface_default.xml" );
+	loaded_ok &= left_eye_classifier.load( base + "/haarcascade_lefteye_2splits.xml" );
+	loaded_ok &= right_eye_classifier.load( base + "/haarcascade_righteye_2splits.xml" );
+
+	return loaded_ok;
+}
 
 int main( int argc, char *argv[] ) {
 	using namespace openni;
@@ -117,9 +166,17 @@ int main( int argc, char *argv[] ) {
 	if( initializeDepthCamera( device, depth, colour ) != 0 ) {
 		return 1;
 	}
-
-
 	VideoFrameRef frame;
+
+
+	// Load classifiers
+	cv::CascadeClassifier faceClassifier, leftEyeClassifier, rightEyeClassifier;
+	if( !loadHaarClassifiers( faceClassifier, leftEyeClassifier, rightEyeClassifier ) ) {
+		std::cout << "Failed to load classfiers" << std::endl;
+		return -1;
+	}
+
+
 
 	// Create a window to display RGB feed
 	cv::namedWindow( "RGB" );
@@ -151,9 +208,47 @@ int main( int argc, char *argv[] ) {
 			// Manage BGR to RGB conversion
 			cv::cvtColor( rgbImage, rgbImage, CV_BGR2RGB);
 
+			// Make a grey version for face detection
+			cv::Mat greyImage( frame.getHeight(), frame.getWidth(), CV_8UC1 );
+			cv::cvtColor( rgbImage, greyImage, CV_RGB2GRAY);
+
+
+			// Find the face
+			std::vector<cv::Rect> faces;
+			cv::Size desiredSize( frame.getWidth() * FACE_SIZE_RATIO, frame.getHeight() * FACE_SIZE_RATIO );
+			faceClassifier.detectMultiScale( greyImage, faces, 1.1, 3, 0, desiredSize );
+
+			// Render if there is one
+			if( faces.size() > 0 ) {
+				// Render face box into image
+				cv::rectangle( rgbImage, faces[0], BLUE, 3 );
+
+				// And find eyes
+				cv::Rect eyeRegion( faces[0].x, faces[0].y, faces[0].width, faces[0].height * EYE_REGION_HEIGHT_PROPORTION );
+				cv::Mat eyeROI = greyImage( eyeRegion );
+				cv::Size desiredSize( eyeRegion.width * EYE_SIZE_RATIO, eyeRegion.width * EYE_SIZE_RATIO );
+
+				std::vector<cv::Rect> leftEyes;
+				leftEyeClassifier.detectMultiScale( eyeROI, leftEyes, 1.1, 2, 0, desiredSize );
+				if( leftEyes.size() > 0 ) {
+					leftEyes[0].x += faces[0].x;
+					leftEyes[0].y += faces[0].y;
+					cv::rectangle( rgbImage, leftEyes[0], GREEN, 2 );
+				}
+
+				std::vector<cv::Rect> rightEyes;
+				rightEyeClassifier.detectMultiScale( eyeROI, rightEyes, 1.1, 2, 0, desiredSize );
+				if( rightEyes.size() > 0 ) {
+					rightEyes[0].x += faces[0].x;
+					rightEyes[0].y += faces[0].y;
+					cv::rectangle( rgbImage, rightEyes[0], GREEN, 2 );
+				}
+			}
+
 
 			// Render
 			cv::imshow( "RGB", rgbImage );
+
 			numColourFrames++;
 		}
 
